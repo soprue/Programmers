77ebf620dfce12938844f9a90b7828767ee18fe0
Gahee Kim <62260343+soprue@users.noreply.github.com>
2022-11-18 00:09:42 +0900

    프로그래머스 URL, 문제 본문 첨부

--- Lv. 0/겹치는 선분의 길이.js
+++ Lv. 0/겹치는 선분의 길이.js
@@ -1,3 +1,11 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120876
+
+// 빨간색, 초록색, 파란색 선분이 x축 위에 있습니다.
+// 세 선분의 x좌표 시작과 끝이 [[start, end], [start, end], [start, end]] 형태로 들어있는 2차원 배열 lines가 매개변수로 주어질 때,
+// 두 개 이상의 선분이 겹치는 부분의 길이를 return 하도록 solution 함수를 완성해보세요.
+
+
+
 function solution(lines) {
     let totalOverlappedLength = 0;
     let min = Math.min(...lines.flat());
